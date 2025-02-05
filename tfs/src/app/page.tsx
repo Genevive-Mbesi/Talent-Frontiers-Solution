e4512547d4008f58433f615120291a2967ff6d3f
mbesi <genmbesi@gmail.com>
2025-02-05 12:46:19 +0300

    Nav

--- tfs/src/app/page.tsx
+++ tfs/src/app/page.tsx
@@ -1,33 +1,30 @@
 import React from "react";
 import Image from "next/image";
-import cover from "@/app/components/Images/cover.jpg"
-
+import cover from "@/app/components/Images/cover.jpg";
 
 export default function Home() {
   return (
     <div className="relative min-h-screen w-full">
-      <Image
-        src={cover}
-        alt="Cover image"
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-        priority
-        className="z-[-1]"
-      />
-
-      <div className="pl-12 pt-12">
-        <h1 className="text-5xl font-bold text-white animate-wave">
-          
+      {/* Red background section */}
+      <div className="bg-red-900 py-12">
+        <h1 className="text-2xl font-bold text-white animate-wave text-center">
+          WE ARE TALENT FRONTIERS SOLUTIONS, A KENYAN-BASED EMPLOYMENT AGENCY DEDICATED TO CONNECTING PROFESSIONAL LOCAL TALENT WITH GLOBAL OPPORTUNITIES.
         </h1>
       </div>
-      <div className="relative z-10 flex  text-3xl flex-col pt-14 pl-12 min-h-screen">
-      
 
-        <div className="absolute bottom-0 left-0 right-0 overflow-hidden leading-none">
-         
-        </div>
+      {/* Cover image below the red background */}
+      <div className="relative min-h-screen">
+        <Image
+          src={cover}
+          alt="Cover image"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          priority
+          className="z-[-1]"
+        />
       </div>
+      
     </div>
   );
 }

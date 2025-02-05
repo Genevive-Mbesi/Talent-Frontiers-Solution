e3adec40d08c838cf5d2c19fd9bfa8bac15ad470
mbesi <genmbesi@gmail.com>
2025-02-05 12:27:39 +0300

    Nav

---
+++ tfs/src/app/Navbar/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Logo from "@/app/components/Images/tfslogo.png";
+import Image from "next/image";
+
+const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-white text-red-700 border-b pt-11 border-gray-200 h-32">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          {/* Logo */}
+          <div className="flex-shrink-0">
+            <Link href="/" aria-label="Home">
+              <Image
+                src={Logo}
+                alt="Cover image"
+                width={200}
+                height={100}
+                objectFit="cover"
+                className="absolute left-10 top-5 z-20 mb-10"
+              />
+            </Link>
+          </div>
+
+          {/* Desktop Navigation */}
+          <div className="hidden md:flex space-x-4">
+            <Link
+              href="/"
+              className="hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium transition"
+              aria-label="Navigate to Home"
+            >
+              WHO WE ARE
+            </Link>
+            <Link
+              href="/Skills"
+              className="hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium transition"
+              aria-label="View Projects"
+            >
+              SERVICES
+            </Link>
+            <Link
+              href="/contact"
+              className="hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium transition"
+              aria-label="Contact Page"
+            >
+              CAREERS
+            </Link>
+            <Link
+              href="/Skills"
+              className="hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium transition"
+              aria-label="View Projects"
+            >
+              CONTACT US
+            </Link>
+            <Link
+              href="/Skills"
+              className="hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium transition"
+              aria-label="View Projects"
+            >
+              LOCATION
+            </Link>
+          </div>
+
+          {/* Mobile Menu Button */}
+          <div className="md:hidden flex items-center justify-end ml-auto">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="text-red-800 focus:outline-none"
+              aria-label="Toggle menu"
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden bg-gray-50 w-fit ml-auto rounded-md shadow-md">
+          <Link
+            href="/"
+            className="block px-4 py-2 hover:bg-gray-100 text-sm font-medium transition"
+            aria-label="Navigate to Home"
+          >
+            WHO WE ARE
+          </Link>
+          <Link
+            href="/Skills"
+            className="block px-4 py-2 hover:bg-gray-100 text-sm font-medium transition"
+            aria-label="View Projects"
+          >
+            SERVICES
+          </Link>
+          <Link
+            href="/contact"
+            className="block px-4 py-2 hover:bg-gray-100 text-sm font-medium transition"
+            aria-label="Contact Page"
+          >
+            CAREERS
+          </Link>
+          <Link
+            href="/contact"
+            className="block px-4 py-2 hover:bg-gray-100 text-sm font-medium transition"
+            aria-label="Contact Page"
+          >
+            CONTACT US
+          </Link>
+          <Link
+            href="/contact"
+            className="block px-4 py-2 hover:bg-gray-100 text-sm font-medium transition"
+            aria-label="Contact Page"
+          >
+            LOCATION
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;

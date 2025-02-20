3a95399cfa45729d5ba0f0689c141499183c692e
mbesi <genmbesi@gmail.com>
2025-02-20 10:02:12 +0300

    layout

--- tfs/src/app/About/page.tsx
+++ tfs/src/app/About/page.tsx
@@ -5,18 +5,21 @@ import wood from "@/app/components/Images/wood.jpg";
 import warehouse from "@/app/components/Images/warehouse.jpg";
 import electrician from "@/app/components/Images/electrician.jpg";
 import waitress from "@/app/components/Images/waitresses.jpg";
+import Slide from "@/components/Slide"
 
 const About: React.FC = () => {
   return (
     <div>
+      <Slide>
     <div className="bg-red-900 z-10 py-12">
         <h1 className="text-2xl font-bold text-white animate-wave text-center">
         <AnimatedText text='WHY CHOOSE US'/>
         </h1>
       </div>
 
-    <div className="max-w-7xl mx-auto px-2 py-12 grid grid-cols-1 md:grid-cols-2 gap-8">
-      <div className="grid grid-cols-2 grid-rows-2 gap-2">
+    <div className="max-w-7xl mx-auto px-2  py-12 grid grid-cols-1 md:grid-cols-2 gap-8">
+      <Slide>
+      <div className="grid grid-cols-2 grid-rows-2 gap-2 pt-12">
         <div className="relative w-full h-60">
           <Image
             src={wood}
@@ -49,10 +52,10 @@ const About: React.FC = () => {
             className="rounded-lg object-cover"
           />
         </div>
-       
       </div>
-
+      </Slide>
       {/* Right Column: About Us Text */}
+      <Slide>
       <div className="flex flex-col justify-center">
        
         <div className="max-w-6xl mx-auto px-6 py-12">
@@ -78,7 +81,9 @@ const About: React.FC = () => {
         </ul>
       </div>
       </div>
+      </Slide>
     </div>
+    </Slide>
     </div>
   );
 };
